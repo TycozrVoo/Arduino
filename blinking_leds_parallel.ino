0d88fb3b99b018029e4185e223cb40cc204e09d4
Tycozr Voo <96928563+TycozrVoo@users.noreply.github.com>
2022-02-13 16:50:34 -0500

    Update blinking_leds_parallel.ino

--- blinking_leds_parallel.ino
+++ blinking_leds_parallel.ino
@@ -1,3 +1,5 @@
+// This is a parallel circuit, which means that all of the components here are connected electrically in one way or another. There is more than one way for electricity to get to the components.
+
 /*
 Parts needed:
 x1 Arduino
@@ -9,7 +11,15 @@ x8 Wires - colors of your choice
 
 /*
 Instructions:
-coming soon
+First, connect one of your wires from the GND pin of your Arduino to the breadboard.
+Then, connect a wire from a pin of your choice on the Arduino to anywhere on the breadboard.
+Now place an LED anywhere you want on the breaboard.
+Connect a wire from the anode of that LED to the wire connected to a pin on the Arduino.
+Place a resistor down with one of its terminals next to the LED. Connect it (with a wire) to the slot next to the wire connected to the GND pin of the Arduino.
+Repeat the previous 3 steps for the other two LEDs.
+Finally, copy - paste the code below into your Arduino IDE. Tailor it to your needs. When you run the code, the LEDs should turn on and start blinking.
+
+Note: You can set up your circuit in any way you want, this is just to direct you in a general idea of how to assemble it.
 */
 
 
